af38469451876ce9f5dc864bcb294155aef31c92
James Meyer <jmeyer@jamess-mbp-2.attlocal.net>
2018-05-23 19:37:30 -0500

    Initial commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,7 +21,7 @@ class About extends Component {
                           <h2 className="background">About Me</h2>
                           <p>I'm a professional fullstack developer. My passion is designing networked systems and computer science. I have more than eight years working experience in this field.</p>
                           <p>I have experience in the defense industry, finance, e-commerce, consulting, file storage and mobile app development. I have experience managing smaller teams to teams of up to 20 developers.</p>
-                          <p>If you have a time sensitive or high profile deployment I am your guy!</p> <a className="button" href="JamesMeyerResume.pdf" download>Download CV</a></div>
+                          <p>If you have a time sensitive or high profile deployment I am your guy!</p> <a className="button" href="https://raw.github.com/nebulr/nebulr.github.io/master/static/media/JamesMeyerResume.4S32Df3D2d.pdf" download>Download CV</a></div>
                   </div>
               </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
@@ -8,5 +10,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
